88f768286cf371da076ff43248db9b250d1d4d76
Valdy Ramadhan <valdyramadhann@gmail.com>
2022-05-19 11:00:32 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#003962',
+        dark: '#0f172a',
+        gmail: '#ea4335',
+        instagram: '#e100b3',
+        linkedIn: '#0A66C2',
+        github: '#181717',
+      },
+    },
+  },
+  plugins: [],
+}
